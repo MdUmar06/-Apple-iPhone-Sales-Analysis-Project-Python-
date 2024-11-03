9cc8cf00340e7fc04ae912bfaed447f3eae9105c
Md_Umar_06 <132151040+MdUmar06@users.noreply.github.com>
2024-11-04 03:07:03 +0530

    Add files via upload

---
+++ Project_1.py
@@ -0,0 +1,15 @@
+# Project_1...
+
+# Apple I phone  Sales Analysis Project...
+
+# Data Analytics Project Life Cycle...
+
+"""
+1. Objective.
+2. Understanding The Data.
+3. Data Cleaning & Data Transformation.
+4. Data Enhancement.
+5. Data Analytics.
+6. Data Visualization.
+
+"""
